7cbff9eeee0eb3ce3541a781431222d270e18168
yaron1TAU <102232163+yaron1TAU@users.noreply.github.com>
2022-08-22 02:08:38 +0300

    Add files via upload

---
+++ Panic Button development/background.js
@@ -0,0 +1,16 @@
+chrome.runtime.onMessage.addListener(sendTabs);     //creates listener for communication with content script
+function sendTabs(request, sender, sendResponse) {
+
+  if (request.msg == "print"){            //prints to console for tests
+    console.log(request.content);
+  }
+
+  else if (request.msg == "closeTab"){    //closes current tab
+    chrome.tabs.query({currentWindow: true ,active: true}, closeTab);
+  }
+}
+
+function closeTab(currTab){               //closes passed tab from query
+  console.log(currTab[0].id);
+  chrome.tabs.remove(currTab[0].id);
+}
